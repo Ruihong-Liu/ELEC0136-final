dab31b150c18583b772e5fb3016e80b9f104e2dc
Ruihong-Liu <147445355+Ruihong-Liu@users.noreply.github.com>
2024-01-14 22:10:24 +0000

    main file for task2

--- Tasks/Task2_main.py
+++ Tasks/Task2_main.py
@@ -1,5 +1,6 @@
 from Task2_1 import up_mongoDB
-from ..Task1.Task1_main import main1
+from Task1_main import main1
 #requests data useing task 1 code
 stock_data,news_data=main1()
+#upload data to MongoDB
 up_mongoDB(stock_data,news_data)
