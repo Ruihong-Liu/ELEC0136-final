7a8d613329b04cda0918ddc61f7f809695817e00
Ruihong-Liu <147445355+Ruihong-Liu@users.noreply.github.com>
2024-01-14 23:00:54 +0000

    Task3_main update

--- Tasks/Task3_main.py
+++ Tasks/Task3_main.py
@@ -1,5 +1,5 @@
 from task3_1 import load_MongoDB_stock,filter_stock_data,missing_check,check_holidays_weekend,none_data_check,reconstucted_data
-from task3_2 import plot_stock_data,plot_price_boxplot,qq_plot,scatter_plot_volume_close,plot_volume_time
+from task3_2 import plot_stock_data,plot_volume_time
 from task3_3 import normalize_data,pca
 
 # load stock data from MongoDB
@@ -30,18 +30,6 @@ plot_stock_data(reconstucted,path='images/Task 3.1 Reconstructed Stock Price Dat
 plot_volume_time(Filtered_data,path='images/Task 3.1 Volume Data.png')
 #plot the volume data of reconstructed data
 plot_volume_time(reconstucted,path='images/Task 3.1 Reconstructed Volume Data.png')
-#plot the boxplot of price
-plot_price_boxplot(Filtered_data,path='images/Task 3.2 Boxplot for Open, High, Low, and Close Prices.png')
-#plot the boxplot of reconstructed price
-plot_price_boxplot(reconstucted,path='images/Task 3.2 Boxplot for Reconstructed Open, High, Low, and Close Prices.png')
-#plot the qq plot of price
-qq_plot(Filtered_data,path='images/Task 3.2 Q-Q Plot of Close.png')
-#plot the qq plot of reconstructed price
-qq_plot(reconstucted,path='images/Task 3.2 Q-Q Plot of Reconstructed Close.png')
-#plot the scatter plot of volume and close price of origional data
-scatter_plot_volume_close(Filtered_data,path='images/Task 3.2 Scatter Plot of Volume and Close.png')
-#plot the scatter plot of volume and close price of reconstructed data
-scatter_plot_volume_close(reconstucted,path='images/Task 3.2 Scatter Plot of Reconstructed Volume and Close.png')
 #normalize the data
 data_norm_ori=normalize_data(Filtered_data)
 #normalize the reconstructed data
