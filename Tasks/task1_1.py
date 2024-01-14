912b022ef36bd485c2a4347e42453b1933171d28
Ruihong-Liu <147445355+Ruihong-Liu@users.noreply.github.com>
2024-01-14 22:08:33 +0000

    task1_1 request data from Web API

--- Tasks/task1_1.py
+++ Tasks/task1_1.py
@@ -3,6 +3,7 @@ task1_1 request of the stock data
 """
 ##import the libaray of requests and json
 import requests
+import json
 ##creat the function that use to apply the data
 def aqu_stock_data(symbol,api_key):
     ##endpoint API from alpha vantage
@@ -21,4 +22,8 @@ def aqu_stock_data(symbol,api_key):
     response = requests.get(url,params)
     ##check if the data requried is successed
     if response.status_code == 200:
-        return response.json()
+        data=response.json()
+        with open("data/stock_data.json", "w") as file:
+            json.dump(data, file)
+        return data
+
