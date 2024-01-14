91e7682fd5f607d2df5b9ba89d3d95e26be1be19
Ruihong-Liu <147445355+Ruihong-Liu@users.noreply.github.com>
2024-01-14 22:09:30 +0000

    main file for task 1

--- Tasks/Task1_main.py
+++ Tasks/Task1_main.py
@@ -1,4 +1,3 @@
-import json
 from task1_1 import aqu_stock_data
 from task1_2 import get_news_data
 def main1():
@@ -7,19 +6,7 @@ def main1():
     #api_key='TVQPPAU7D3SY0U1R'
     ##chose the company symbol
     symbol  =   "MSFT"
-    ##require data
+    ##request data
     stock_data = aqu_stock_data(symbol,api_key)
     news_data = get_news_data(symbol,api_key)
-    if stock_data:
-        # save the response content to a file
-        with open("Task1/Microsoft Original.json", "w") as f:
-            json.dump(stock_data, f)
-
-        # read the response content from the file
-        with open("Task1/Microsoft Original.json", "r") as f:
-            stock_data_loaded = json.load(f)
-            assert stock_data == stock_data_loaded
-            print(stock_data_loaded)
-    else:
-        print("data requests fail")
     return stock_data,news_data
