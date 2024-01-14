56d91c8dd1d68b67f1196fca2648d8d4c3c4498f
Ruihong-Liu <147445355+Ruihong-Liu@users.noreply.github.com>
2024-01-14 22:08:59 +0000

    task1_2 news data requests

--- Tasks/task1_2.py
+++ Tasks/task1_2.py
@@ -2,7 +2,7 @@
 require stock news data
 """
 import requests
-import json
+
 def get_news_data(symbol,api_key):
     #URL for Using news API
     url = "https://www.alphavantage.co/query"
@@ -11,9 +11,11 @@ def get_news_data(symbol,api_key):
         "function":"NEWS_SENTIMENT",
         #choosing company
         "tickers":symbol,
+        #limit the number of news
+        "limit":"1000",
         #time stating
         "time_from":"20190401T0000",
-        #
+        #time ending
         "time_to":"20230331T0000",
         ##user API key
         "apikey":api_key
@@ -22,18 +24,6 @@ def get_news_data(symbol,api_key):
     # Check if the response is successful
     if response.status_code == 200:
         data = response.json()
-        if data:
-            # save the response content to a file
-            with open("Task1/Microsoft Original news.json", "w") as f:
-                json.dump(data, f)
-
-            # read the response content from the file
-            with open("Task1/Microsoft Original news.json", "r") as f:
-                stock_data_loaded = json.load(f)
-                assert data == stock_data_loaded
-                print(stock_data_loaded)    
     else:
         print("cannot requests from API, status code:", response.status_code)
-        
-    
-    return response
+    return data
