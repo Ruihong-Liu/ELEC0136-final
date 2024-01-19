ed42d47ed4170e3679d7bd017c0e155f4038d5bb
Ruihong-Liu <147445355+Ruihong-Liu@users.noreply.github.com>
2024-01-19 11:09:37 +0000

    task3.3 renew

--- Tasks/task3_3.py
+++ Tasks/task3_3.py
@@ -1,17 +1,12 @@
-from task3_1 import filter_stock_data,load_MongoDB_stock
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-stock_price=load_MongoDB_stock()
-Filtered_data=filter_stock_data(stock_price)
 def normalize_data(data):
     scaler = MinMaxScaler()
     normalized_data = scaler.fit_transform(data)
     normalized_df = pd.DataFrame(normalized_data, columns=data.columns, index=data.index)
     return normalized_df
-data_norm=normalize_data(Filtered_data)
-print(data_norm)
 
 
 def pca(filtered_data):
@@ -31,5 +26,4 @@ def pca(filtered_data):
 
     # threshold of 90% variance explained
     n_components = (cumulative_explained_variance < 0.90).sum() + 1
-
-pca(data_norm)
+    return n_components
